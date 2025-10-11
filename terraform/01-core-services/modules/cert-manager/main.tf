1e758dd9670b56262bf5be06351069da9ea09f35
ikabbash <i.kabbash21@gmail.com>
2025-10-11 15:47:22 +0300

    refactor: cert-manager module main.tf

--- terraform/01-core-services/modules/cert-manager/main.tf
+++ terraform/01-core-services/modules/cert-manager/main.tf
@@ -1,3 +1,11 @@
+locals {
+  clusterissuer_manifest = templatefile("${path.module}/templates/clusterissuer.yaml.tftpl", {
+    cluster_issuer_name    = var.cluster_issuer_name
+    letsencrypt_email      = var.letsencrypt_email
+    cloudflare_secret_name = var.cloudflare_secret_name
+  })
+}
+
 resource "helm_release" "cert_manager" {
   name             = "cert-manager"
   repository       = "https://charts.jetstack.io"
@@ -32,32 +40,20 @@ resource "null_resource" "letsencrypt_cluster_issuer" {
     cluster_issuer_name    = var.cluster_issuer_name
     letsencrypt_email      = var.letsencrypt_email
     cloudflare_secret_name = var.cloudflare_secret_name
-    manifest_content = templatefile("${path.module}/templates/clusterissuer.yaml.tftpl", {
-      cluster_issuer_name    = var.cluster_issuer_name
-      letsencrypt_email      = var.letsencrypt_email
-      cloudflare_secret_name = var.cloudflare_secret_name
-    })
+    manifest_content       = local.clusterissuer_manifest
   }
 
   provisioner "local-exec" {
-    command = <<-EOT
-      cat <<EOF | kubectl apply -f -
-      ${templatefile("${path.module}/templates/clusterissuer.yaml.tftpl", {
-    cluster_issuer_name    = var.cluster_issuer_name
-    letsencrypt_email      = var.letsencrypt_email
-    cloudflare_secret_name = var.cloudflare_secret_name
-})}
-      EOF
-    EOT
-}
+    command = "echo '${local.clusterissuer_manifest}' | kubectl apply -f -"
+  }
 
-provisioner "local-exec" {
-  when    = destroy
-  command = "kubectl delete clusterissuer letsencrypt-prod --ignore-not-found=true"
-}
+  provisioner "local-exec" {
+    when    = destroy
+    command = "kubectl delete clusterissuer ${self.triggers.cluster_issuer_name} --ignore-not-found=true"
+  }
 
-depends_on = [
-  helm_release.cert_manager,
-  kubernetes_secret_v1.cloudflare_kubernetes_secret
-]
+  depends_on = [
+    helm_release.cert_manager,
+    kubernetes_secret_v1.cloudflare_kubernetes_secret
+  ]
 }
