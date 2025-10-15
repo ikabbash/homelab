6b9e60b8031dbf6ff25dcce1f768f73bd3ba939e
ikabbash <i.kabbash21@gmail.com>
2025-10-15 21:31:50 +0300

    update: vault infra kv-v2 secret engine mounts

--- terraform/02-vault-setup/main.tf
+++ terraform/02-vault-setup/main.tf
@@ -8,3 +8,15 @@ terraform {
     }
   }
 }
+
+resource "vault_mount" "homelab_infra_kv" {
+  path        = "homelab/infra/kv-secret"
+  type        = "kv-v2"
+  description = "KV-v2 secret mount for homelab infrastructure"
+}
+
+resource "vault_mount" "homelab_apps_kv" {
+  path        = "homelab/apps/kv-secret"
+  type        = "kv-v2"
+  description = "KV-v2 secret mount for homelab applications"
+}
