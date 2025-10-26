b32c11c575c6f99a9e980d0d79494f19ce26d942
ikabbash <i.kabbash21@gmail.com>
2025-10-26 21:20:58 +0300

    chores: delete old doc and unneeded 02-vault-setup module outputs

--- terraform/02-vault-setup/outputs.tf
+++ terraform/02-vault-setup/outputs.tf
@@ -3,11 +3,6 @@ output "infra_kv_mount_path" {
   value       = module.vault_setup.infra_kv_mount_path
 }
 
-output "apps_kv_mount_path" {
-  description = "Path of the KV-v2 secret mount for homelab applications"
-  value       = module.vault_setup.apps_kv_mount_path
-}
-
 output "vso_role_name" {
   description = "Name of the Kubernetes auth backend role for VSO"
   value       = module.vault_setup.vso_role_name
@@ -16,11 +11,6 @@ output "vso_role_name" {
 output "kubernetes_auth_path" {
   description = "Path where the VSO Kubernetes auth backend is enabled"
   value       = module.vault_setup.kubernetes_auth_path
-}
-
-output "vso_audience" {
-  description = "Audience configured for the VSO Kubernetes auth role"
-  value       = module.vault_setup.vso_audience
 }
 
 output "vso_service_account" {
