d8ea5ae4eb56b672767b0387086c9b9153b7ed63
ikabbash <i.kabbash21@gmail.com>
2025-11-12 20:45:51 +0200

    update: Talos config script refactor

--- talos/setup.sh
+++ talos/setup.sh
@@ -2,24 +2,21 @@
 
 set -e
 
-# Variables
 BASE_DIR=$(readlink -f $(dirname ${0}))
-OUT_DIR="${BASE_DIR}/_out"
+CLUSTER_NAME="homelab-cluster"
+OUT_DIR="${BASE_DIR}/_out/${CLUSTER_NAME}"
 CONTROLPLANES_DIR="${OUT_DIR}/controlplanes"
 WORKERS_DIR="${OUT_DIR}/workers"
 CLUSTER_CONFIG_DIR="${OUT_DIR}/cluster-config"
+TALOS_CONFIG="${CLUSTER_CONFIG_DIR}"/talosconfig
+KUBE_CONFIG="${CLUSTER_CONFIG_DIR}"/kubeconfig
 
-CLUSTER_NAME="homelab-cluster"
+PATCH_FILE="${BASE_DIR}/cluster-patch.yaml"
 VIRTUAL_IP_ADDRESS="192.168.1.200"
-NETWORK_GATEWAY="192.168.1.1" # For now presuming there is only one
+NETWORK_GATEWAY="192.168.1.1" # Presuming there is only one
 
-# Check if at least one IP was provided
-if [ "$#" -eq 0 ]; then
-    echo "Usage: $0 <IP1> [IP2] [IP3] ..."
-    exit 1
-fi
-# Store all arguments in an array
-CONTROL_PLANE_NODES=("$@")
+CONTROL_PLANE_NODES=("192.168.1.106")
+# WORKER_NODES=("192.168.1.104" "192.168.1.105")
 
 # Generate secrets if the file doesn't exist
 if [ ! -f "${CLUSTER_CONFIG_DIR}/secrets.yaml" ]; then
@@ -35,55 +32,50 @@ for dir in "$CONTROLPLANES_DIR" "$WORKERS_DIR"; do
     fi
 done
 
-# Generate controlplane configs
+# Check if the cluster is just a single control plane node
 if [ ${#CONTROL_PLANE_NODES[@]} -eq 1 ]; then
-    node_ip="${CONTROL_PLANE_NODES[0]}"
-    output_file="${CONTROLPLANES_DIR}/controlplane-1.yaml"
-
-    echo "Generating controlplane-1.yaml for node ${node_ip}..."
-
-    talosctl gen config "${CLUSTER_NAME}" "https://${node_ip}:6443" \
-        --with-secrets "${CLUSTER_CONFIG_DIR}/secrets.yaml" \
-        --config-patch-control-plane @"${BASE_DIR}/single-node-patch.yaml" \
-        --output-types controlplane \
-        --output "${output_file}"
-
-    sed -i "s/NODE_HOSTNAME/talos-cp-1/g" ${output_file}
-    sed -i "s/NODE_IP_ADDRESS/${node_ip}/g" ${output_file}
-
-    talosctl get disks --insecure -o table --nodes ${node_ip}
-    read -p "Enter the device to use for talos-cp-1 (e.g. /dev/sda) — WARNING: it will be completely wiped: " chosen_disk
-    sed -i "s|NODE_DISK_DEVICE|${chosen_disk}|g" ${output_file}
-
-else
-    count=1
-    for i in "${CONTROL_PLANE_NODES[@]}"; do
-        output_file="${CONTROLPLANES_DIR}/controlplane-${count}.yaml"
-
-        echo "Generating controlplane-${count}.yaml for node ${node_ip}..."
-
-        talosctl gen config "${CLUSTER_NAME}" "https://${VIRTUAL_IP_ADDRESS}:6443" \
-            --with-secrets "${CLUSTER_CONFIG_DIR}/secrets.yaml" \
-            --config-patch-control-plane @"${BASE_DIR}/cluster-patch.yaml" \
-            --output-types controlplane \
-            --output "${output_file}"
-
-        sed -i "s/NODE_HOSTNAME/talos-cp-$count/g" ${output_file}
-        sed -i "s/VIRTUAL_IP_ADDRESS/${VIRTUAL_IP_ADDRESS}/g" ${output_file}
-        sed -i "s/NODE_IP_ADDRESS/${i}/g" ${output_file}
-
-        talosctl get disks --insecure -o table --nodes $i
-        read -p "Enter the device to use for talos-cp-$count (e.g. /dev/sda) — WARNING: it will be completely wiped: " chosen_disk
-        sed -i "s|NODE_DISK_DEVICE|${chosen_disk}|g" ${output_file}
-
-        ((count++))
-    done
+    VIRTUAL_IP_ADDRESS="${CONTROL_PLANE_NODES[0]}"
+    PATCH_FILE="${BASE_DIR}/single-node-patch.yaml"
 fi
+
 # Generate talosconfig
 if [ ! -f "${CLUSTER_CONFIG_DIR}/talosconfig" ]; then
     talosctl gen config "${CLUSTER_NAME}" "https://${VIRTUAL_IP_ADDRESS}:6443" \
         --with-secrets "${CLUSTER_CONFIG_DIR}/secrets.yaml" \
         --config-patch-control-plane @"${BASE_DIR}/cluster-patch.yaml" \
         --output-types talosconfig \
-        --output "${CLUSTER_CONFIG_DIR}"/talosconfig
+        --output "${TALOS_CONFIG}"
+    
+    talosctl --talosconfig="${TALOS_CONFIG}" config endpoints "${VIRTUAL_IP_ADDRESS}"
+    talosctl --talosconfig="${TALOS_CONFIG}" config node "${VIRTUAL_IP_ADDRESS}"
+fi
+
+# Generate controlplane configs
+count=1
+for i in "${CONTROL_PLANE_NODES[@]}"; do    
+    output_file="${CONTROLPLANES_DIR}/controlplane-${count}.yaml"
+    echo "Generating controlplane-${count}.yaml for node ${node_ip}..."
+    
+    talosctl gen config "${CLUSTER_NAME}" "https://${VIRTUAL_IP_ADDRESS}:6443" \
+            --with-secrets "${CLUSTER_CONFIG_DIR}/secrets.yaml" \
+            --config-patch-control-plane @"${PATCH_FILE}" \
+            --output-types controlplane \
+            --output "${output_file}"
+    
+    sed -i "s/NODE_HOSTNAME/talos-cp-${count}/g" ${output_file}
+    sed -i "s/VIRTUAL_IP_ADDRESS/${VIRTUAL_IP_ADDRESS}/g" ${output_file}
+    sed -i "s/NODE_IP_ADDRESS/${i}/g" ${output_file}
+
+    if ! talosctl get disks --insecure -o table --nodes "${i}" 2>/dev/null; then
+        talosctl get disks --talosconfig "$TALOS_CONFIG" -o table --nodes "${i}" || exit 1
+    fi
+    read -p "Enter the device to use for talos-cp-${count} (e.g. /dev/sda) — WARNING: it will be completely wiped: " chosen_disk
+    sed -i "s|NODE_DISK_DEVICE|${chosen_disk}|g" ${output_file}
+
+    ((count++))
+done
+
+# Generate kubeconfig
+if [ ! -f "${CLUSTER_CONFIG_DIR}/kubeconfig" ]; then
+    talosctl --talosconfig "${TALOS_CONFIG}" kubeconfig "${KUBE_CONFIG}"
 fi
