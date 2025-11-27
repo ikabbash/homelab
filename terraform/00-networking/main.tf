70a4af357d3aafe1f2e05a3757e229a0b99ccf86
ikabbash <i.kabbash21@gmail.com>
2025-11-27 16:00:32 +0200

    BREAKING CHANGE: replace F5 NGINX Ingress Controller with Cilium Gateway API

--- terraform/00-networking/main.tf
+++ terraform/00-networking/main.tf
@@ -14,10 +14,39 @@ terraform {
 }
 
 locals {
-  cilium_l2_policy_manifest = templatefile("${path.module}/templates/ingress-controller-l2-policy.yaml.tftpl", {
-    loadbalancer_ip = var.ingress_controller_loadbalancer
+  cilium_l2_policy_manifest = templatefile("${path.module}/templates/cilium-lb-l2-policy.yaml.tftpl", {
+    policy_name  = var.cilium_l2_policy_name
+    gateway_name = var.gateway_name
   })
+
+  ip_pool_name = "${var.gateway_name}-ip-pool"
+  cilium_gateway_manifest = templatefile("${path.module}/templates/cilium-gateway.yaml.tftpl", {
+    gateway_name    = var.gateway_name
+    loadbalancer_ip = var.lb_service_external_ip
+    ip_pool_name    = local.ip_pool_name
+  })
+
+  gateway_api_base = "https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/${var.gateway_api_version}/config/crd"
 }
+
+# Apply Gateway API CRDs
+resource "null_resource" "gateway_api_crds" {
+  triggers = {
+    version = var.gateway_api_version
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl apply -f ${local.gateway_api_base}/standard/gateway.networking.k8s.io_gatewayclasses.yaml
+      kubectl apply -f ${local.gateway_api_base}/standard/gateway.networking.k8s.io_gateways.yaml
+      kubectl apply -f ${local.gateway_api_base}/standard/gateway.networking.k8s.io_httproutes.yaml
+      kubectl apply -f ${local.gateway_api_base}/standard/gateway.networking.k8s.io_referencegrants.yaml
+      kubectl apply -f ${local.gateway_api_base}/standard/gateway.networking.k8s.io_grpcroutes.yaml
+      kubectl apply -f ${local.gateway_api_base}/experimental/gateway.networking.k8s.io_tlsroutes.yaml
+    EOT
+  }
+}
+
 
 # Deploy Cilium
 resource "helm_release" "cilium" {
@@ -38,27 +67,28 @@ resource "helm_release" "cilium" {
 }
 
 # Deploy F5's NGINX Ingress Controller
-resource "helm_release" "ingress_controller" {
-  name             = "nginx-ingress"
-  repository       = "https://helm.nginx.com/stable/"
-  chart            = "nginx-ingress"
-  namespace        = var.ingress_controller_chart_namespace
-  version          = var.ingress_controller_chart_version
-  skip_crds        = false
-  create_namespace = true
+# resource "helm_release" "ingress_controller" {
+#   name             = "nginx-ingress"
+#   repository       = "https://helm.nginx.com/stable/"
+#   chart            = "nginx-ingress"
+#   namespace        = var.ingress_controller_chart_namespace
+#   version          = var.ingress_controller_chart_version
+#   skip_crds        = false
+#   create_namespace = true
 
-  values = [
-    templatefile("./templates/ingress-controller-values.yaml.tftpl", {
-      loadbalancer_ip = var.ingress_controller_loadbalancer
-    })
-  ]
-  depends_on = [helm_release.cilium]
-}
+#   values = [
+#     templatefile("./templates/ingress-controller-values.yaml.tftpl", {
+#       loadbalancer_ip = var.lb_service_external_ip
+#     })
+#   ]
+#   depends_on = [helm_release.cilium]
+# }
 
 resource "null_resource" "cilium_l2_policy" {
   triggers = {
-    policy_name      = "nginx-ingress-l2-policy"
-    loadbalancer_ip  = var.ingress_controller_loadbalancer
+    policy_name      = var.cilium_l2_policy_name
+    gateway_name     = var.gateway_name
+    ip_pool_name     = local.ip_pool_name
     manifest_content = local.cilium_l2_policy_manifest
   }
 
@@ -73,5 +103,30 @@ resource "null_resource" "cilium_l2_policy" {
   provisioner "local-exec" {
     when    = destroy
     command = "kubectl delete ciliuml2announcementpolicy ${self.triggers.policy_name} --ignore-not-found=true"
+  }
+}
+
+resource "null_resource" "cilium_gateway" {
+  triggers = {
+    gateway_name     = var.gateway_name
+    ip_pool_name     = local.ip_pool_name
+    loadbalancer_ip  = var.lb_service_external_ip
+    manifest_content = local.cilium_gateway_manifest
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      cat <<EOF | kubectl apply -f -
+      ${local.cilium_gateway_manifest}
+      EOF
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<-EOT
+      kubectl delete gateway ${self.triggers.gateway_name} --ignore-not-found=true
+      kubectl delete ciliumloadbalancerippool ${self.triggers.ip_pool_name} --ignore-not-found=true
+    EOT
   }
 }
--- terraform/00-networking/variables.tf
+++ terraform/00-networking/variables.tf
@@ -5,6 +5,18 @@ variable "kubeconfig_path" {
   default     = "~/.kube/config"
 }
 
+variable "gateway_api_version" {
+  description = "Kubernetes Gateway API CRDs version"
+  type        = string
+  default     = "v1.2.0"
+}
+
+variable "gateway_name" {
+  description = "Name of the main Gateway for all namespaces"
+  type        = string
+  default     = "homelab-gw"
+}
+
 variable "cilium_chart_version" {
   description = "Cilium Helm chart version"
   type        = string
@@ -22,19 +34,25 @@ variable "cilium_service_host" {
   type        = string
 }
 
-variable "ingress_controller_chart_version" {
-  description = "nginx-ingress Helm chart version"
+variable "cilium_l2_policy_name" {
+  description = "Cilium L2 policy name for both Ingress Controller and Gateway API"
   type        = string
-  default     = "2.3.1"
+  default     = "cluster-entrypoint-l2-policy"
 }
 
-variable "ingress_controller_chart_namespace" {
-  description = "The Kubernetes namespace to deploy nginx-ingress into"
+variable "lb_service_external_ip" {
+  description = "Load balancer external IP for either Ingress Controller or Gateway API"
   type        = string
-  default     = "nginx-ingress"
 }
 
-variable "ingress_controller_loadbalancer" {
-  description = "Load balancer address for the ingress controller"
-  type        = string
-}
+# variable "ingress_controller_chart_version" {
+#   description = "nginx-ingress Helm chart version"
+#   type        = string
+#   default     = "2.3.1"
+# }
+
+# variable "ingress_controller_chart_namespace" {
+#   description = "The Kubernetes namespace to deploy nginx-ingress into"
+#   type        = string
+#   default     = "nginx-ingress"
+# }
