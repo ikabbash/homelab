fd772b39ddd527d50059bd99bc82801b4eaacff2
ikabbash <i.kabbash21@gmail.com>
2025-12-15 20:09:40 +0200

    update: Terraform phase02 homelab storage path output

--- terraform/phase02/outputs.tf
+++ terraform/phase02/outputs.tf
@@ -37,3 +37,8 @@ output "homelab_domain" {
   description = "Domain name for the homelab environment"
   value       = var.homelab_domain
 }
+
+output "homelab_data_path" {
+  description = "Base path for homelab data storage"
+  value       = var.homelab_data_path
+}
