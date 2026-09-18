c4617c82a3a54ccfc9758332edf65088a43f79db
Ibrahim Kabbash <68027901+ikabbash@users.noreply.github.com>
2026-09-18 15:02:42 +0300

    chore(deps): update helm release authentik to v2026.8.3 (#48)
    Co-authored-by: Renovate Bot <renovate@homelab.local>

--- terraform/phase04/main.tf
+++ terraform/phase04/main.tf
@@ -62,7 +62,7 @@ module "authentik" {
   source                 = "./modules/authentik"
   chart_namespace        = var.authentik_namespace
   # renovate: datasource=helm depName=authentik registryUrl=https://charts.goauthentik.io
-  chart_version          = "2026.8.0"
+  chart_version          = "2026.8.3"
   postgres_secret_name   = module.postgresql.postgres_secret_name
   postgres_host          = module.postgresql.postgres_host
   storage_class_name     = local.phase01.host_storage_class_name
